f33396a346bb9700ae64a98951860acf04086410
Raisa <Raissa.Spiridonova@yandex.ru>
2025-01-08 13:10:01 +0300

    add const to html

--- modal.js
+++ modal.js
@@ -1,17 +1,18 @@
 //  здесь прописана функция для открытия и закрытия модального окна. Выбран один из вариантов, представленных в уроке с использованием jS
 
-const body = document.querySelector('body.page');
-const btnOpenModal = document.querySelector('#modal-1-trigger');
-const modal = document.querySelector('#modal-1');
-const btnCloseModal = document.querySelector('#modal-1-close');
+//const body = document.querySelector('body.page');
+//const btnOpenModal = document.querySelector('#modal-1-trigger');
+//const modal = document.querySelector('#modal-1');
+//const btnCloseModal = document.querySelector('#modal-1-close');
 
 const handleModalSwitch = () => {
+  console.log('handleModalSwitch invoked');
   modal.classList.toggle('modal--closed');
   body.classList.toggle('page--scroll-locked');
 }
 
-btnOpenModal.addEventListener('click', handleModalSwitch)
+//btnOpenModal.addEventListener('click', handleModalSwitch);
 
-btnCloseModal.addEventListener('click', handleModalSwitch);
+//btnCloseModal.addEventListener('click', handleModalSwitch);
 
 
